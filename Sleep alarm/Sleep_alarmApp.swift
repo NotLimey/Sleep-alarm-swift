871931da40acaffec857a346b1e30f36e51f4cf9
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2023-01-02 12:34:12 +0100

    Initial Commit

---
+++ Sleep alarm/Sleep_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_alarmApp.swift
+//  Sleep alarm
+//
+//  Created by Martin Myhre on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
